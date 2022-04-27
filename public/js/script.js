7ed0afc46798fff27a2be9137f9114ff60cf5240
Malkavianson <alexneto.best@hotmail.com>
2022-04-27 12:21:16 -0300

    add shares

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+// function share(){
+	// if (navigator.share) {
+		// navigator.share({
+			// title: '@chiennevictrix',
+			// text: 'Share Chienne Victrix 4 the world',
+			// url: 'http://chiennevictrix.herokuapp.com/',
+		// })
+			// .then(() => console.log('Successful share'))
+			// .catch((error) => console.log('Error sharing', error));
+	// } else {
+    // console.log('Share not supported on this browser, do it the old way.');
+	// }
+// };
