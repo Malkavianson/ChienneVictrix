3dfd652e3380ebc65e53eba9a499c60d12f8193b
malkavianson <63012624+Malkavianson@users.noreply.github.com>
2022-08-20 22:58:43 -0300

    Fix: telegram link

--- public/js/social.js
+++ public/js/social.js
@@ -1,58 +1,59 @@
 export default {
-	social: [
-		{
-			name: 'Privacy',
-			description: 'https://privacy.com.br/Checkout/ChienneVictrix/',
-			picture: 'img/privacy.png',
-			id: 0,
-		},
-		{
-			name: 'My Wishlist',
-			description: 'https://mywishlist.online/w/hqrvlz/chiennes-wishlist',
-			picture: 'img/mywishlist.png',
-			id: 1 ,
-		},
-		{
-			name: 'Packzim',
-			description: 'https://www.packzin.com.br/perfil/chiennevictrix',
-			picture: 'img/packzim.png',
-			id: 2,
-		},
-		{
-			name: 'Tiktok',
-			description: 'https://www.tiktok.com/@chiennevictrix',
-			picture: 'img/tiktok.png',
-			id: 3,
-		},		
-		{
-			name: 'Twitter',
-			description: 'https://twitter.com/ChienneVictrix',
-			picture: 'img/twitter.png',
-			id: 4,
-		},
-		{
-			name: 'Instagram',
-			description: 'https://www.instagram.com/chiennevictrix/',
-			picture: 'img/instagram.png',
-			id: 5,
-		},
-		{
-			name: 'Telegram',
-			description: 'https://t.me/+YWVhn5d8AcJjN2Y5',
-			picture: 'img/telegram.jpg',
-			id: 6,
-		},
-		{
-			name: 'Snapchat',
-			description: 'https://www.snapchat.com/add/chiennevictrix?share_id=yhdoDeWNM2c&locale=pt-BR',
-			picture: 'img/snapchat.png',
-			id: 7,
-		},
-		{
-			name: 'Tumblr',
-			description: 'https://saoko-stuffs.tumblr.com/',
-			picture: 'img/tumblr.jpg',
-			id: 8,
-		},
-	]
-}
+  social: [
+    {
+      name: "Privacy",
+      description: "https://privacy.com.br/Checkout/ChienneVictrix/",
+      picture: "img/privacy.png",
+      id: 0,
+    },
+    {
+      name: "Telegram",
+      description: "https://t.me/ChienneVictrixCanal",
+      picture: "img/telegram.jpg",
+      id: 1,
+    },
+    {
+      name: "Twitter",
+      description: "https://twitter.com/ChienneVictrix",
+      picture: "img/twitter.png",
+      id: 2,
+    },
+    {
+      name: "Tiktok",
+      description: "https://www.tiktok.com/@chiennevictrix",
+      picture: "img/tiktok.png",
+      id: 3,
+    },
+    {
+      name: "Packzim",
+      description: "https://www.packzin.com.br/perfil/chiennevictrix",
+      picture: "img/packzim.png",
+      id: 4,
+    },
+    {
+      name: "Instagram",
+      description: "https://www.instagram.com/chiennevictrix/",
+      picture: "img/instagram.png",
+      id: 5,
+    },
+    {
+      name: "My Wishlist",
+      description: "https://mywishlist.online/w/hqrvlz/chiennes-wishlist",
+      picture: "img/mywishlist.png",
+      id: 6,
+    },
+    {
+      name: "Snapchat",
+      description:
+        "https://www.snapchat.com/add/chiennevictrix?share_id=yhdoDeWNM2c&locale=pt-BR",
+      picture: "img/snapchat.png",
+      id: 7,
+    },
+    {
+      name: "Tumblr",
+      description: "https://saoko-stuffs.tumblr.com/",
+      picture: "img/tumblr.jpg",
+      id: 8,
+    },
+  ],
+};
